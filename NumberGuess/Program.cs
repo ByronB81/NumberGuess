d0de8075ad889c3b50250b96dc7ae4e8ef3df1e4
Byron Blocker <byronblocker@gmail.com>
2017-07-25 14:03:42 -0700

    Add project files.

---
+++ NumberGuess/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberGuess
+{
+    class Program
+    {
+        bool ProgramRunning = true;
+        private int _userNum;
+        private int _comNum;
+
+        //public int SetComNum
+        //{
+        //    set { _comNum}
+        //}
+
+        public static void ComPicksNum()
+        {
+            Random rng = new Random();
+            set { _comNum = rng.Next(1, 10); }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
